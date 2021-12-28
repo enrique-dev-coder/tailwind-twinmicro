fbe280b45725ab33f0df7c82d04c3a42d0cbd3c4
enrique95 <enrique_alvarado95@hotmail.com>
2021-12-28 08:50:05 -0600

    Initial babel setup for ssr styled components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name='viewport' content='width=device-width,initial-scale=1'/>
+      </Head>
+      <Component {...pageProps} />
+    </>
+
+
+
+  )
+  
+  
 }
 
 export default MyApp
